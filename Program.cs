18788a189269241c37b850a80364eb248dbe14db
“battusairam” <battusairam6@gmail.com>
2023-06-09 11:53:51 +0530

    [SAIRAM]: MSTest MoodAnalyzer Problem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MSTest_Mood_Analyzer_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TestProject1-MoodAnalyzer/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using MSTest_Mood_Analyzer_Problem.MoodAnalyzer;
+namespace TestProject1_MoodAnalyzer;
+using System;
+[TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        // Arrange
+        MoodAnalyzerClass obj = new MoodAnalyzerClass("null");
+
+        // Act
+        string expected = "Happy";
+        string actual = obj.AnalyzeMood();
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+        
+        }
+    }
